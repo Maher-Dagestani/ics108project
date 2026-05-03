42676f5b2e9f9c759c2e453d93003f6c9ae1a4f9
MaherDagestani <maher.dagestani9@gmail.com>
2026-05-03 18:45:28 +0300

    Create Department.java

---
+++ Department.java
@@ -0,0 +1,12 @@
+// this is class department
+public class Department {
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
